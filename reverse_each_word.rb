be990fb4c51661c0a497abda92c777828734fc7b
Catherine Salazar <catcreates13@gmail.com>
2018-01-21 03:07:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
 def reverse_each_word(string)
-  string.
+  words = string.split(' ')
+  reverse_str = []
+
+  words.length.times do |i|
+    reverse_str[i] = words[i].reverse
+  end
 
 
-
-
+  return reverse_str.join(" ")
 end
