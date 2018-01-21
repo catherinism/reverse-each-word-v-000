c146f3bca7bfeeed072b77d6599551ea87ec873a
Catherine Salazar <catcreates13@gmail.com>
2018-01-21 03:10:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
 def reverse_each_word(string)
-  words = string.split(' ')
-  reverse_str = []
-
-  words.length.times do |i|
-    reverse_str[i] = words[i].reverse
-  end
-
-
-  return reverse_str.join(" ")
+  new_string = []
 end
